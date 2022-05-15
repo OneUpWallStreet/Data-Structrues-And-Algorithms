b0d3cbd641e5a60dab58cc4740cdce0fbe4d42ab
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-16 00:03:46 +0530

    minor changes + another easy problem

--- Problems/3. Longest Substring Without Repeating Characters.cpp
+++ Problems/3. Longest Substring Without Repeating Characters.cpp
@@ -4,38 +4,33 @@ using namespace std;
 
 
 class Solution {
-    
-
     public:
         int lengthOfLongestSubstring(string s) {
-            
-            int highestCounter = 0;
+
             int i =0;
+            int j = 0;
 
-             for(int j=0;j<s.length();j++){
-                
-                // Not Found we should increase the window
-                if( seen.find(s[j]) == seen.end()){
-                    highestCounter = max(highestCounter,j-i+1);
-                }
-                else{
-                    // check if we have seen the element in current window
-                    if(seen[s[j]]<i){
-                        highestCounter = max(highestCounter,j-i+1);
-                    }
-                    else{
-                        i = seen[s[j]] + 1;
-                    }
-                }
-                seen[s[j]] = j;
-             }
+            int maxLen = 0;
 
-             return highestCounter;
+            unordered_set<int> mySet;
+
+            while(j<s.length()){
+
+                while(mySet.find(s[j]) != mySet.end()){
+                    mySet.erase(mySet.find(s[i]));
+                    i++;
+                }
+
+                mySet.insert(s[j]);
+                maxLen = max(maxLen,j-i+1);
+
+                j++;
+
+            }
+
+            return maxLen;
+
         }
-
-
-    private:
-        unordered_map<char,int> seen;
 };
 
 
@@ -45,8 +40,6 @@ int main(){
 
     string input = "abba";
 
-    cout << "string is: " << input << endl;
-
     Solution solution;
     int answer = solution.lengthOfLongestSubstring(input);
     
@@ -55,69 +48,4 @@ int main(){
 }
 
 
-            // int highest = 0;
-            // int i = 0;
-
-            // for(int j =0;j<s.length();j++){
-            //     if(hashMap[s[j]]  == hashMap.end()){
-            //         hashMap[s[j]] = 0;
-            //     }
-            //     else{
-            //         hashMap[s[j]]++;
-            //     }
-            // }
-
-
-
-            // if(s.length()==0){
-            //     return 0;
-            // }
-            // else if(s.length()==1){
-            //     return 1;
-            // }
-
-            // int i =0;
-            // int j = s.length()-1;
-
-            // int counter = 1;
-            // int highestValue = 1;
-
-
-            // while(i<j){
-            //     if(s[i] != s[j]){
-            //         i++;
-            //     }
-            //     else{
-            //         j--;                    
-            //     }
-            // }
-
-            // while(true){
-
-            //     // cout << "i is:" << i << " & j is: " << j << endl;
-                
-            //     if(s[i] != s[j]){
-            //         cout << "incrementing counter for: " << s[i] << " & "f << s[j] << endl;
-            //         counter++;
-            //         // cout << "counter is: " << counter << endl; 
-            //         j++;
-            //         i++;
-            //         if(j==s.length()){
-            //             break;
-            //         }
-
-            //     }
-            //     else{
-            //         if(counter>highestValue){
-            //             highestValue = counter;
-            //         }
-            //         counter =0;
-            //         i++;
-            //     }
-            // }
-
-            // cout << "i: " << i << " j: " << j << endl;
-  
-            // return highestValue;
-
-            
+    
---
+++ Problems/58. Length of Last Word.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+
+            // Edge Case
+            if(s.length()==1){
+                return 1;
+            }
+
+
+			// Initialise pointer towards end
+			// of string
+            int j = s.length()-1;
+
+			// make sure pointer points to
+			// char and ignores empty space
+            while(s[j] == ' '){
+                j--;
+            }
+
+
+			// counter for word
+            int counter = 0;
+
+			// while pointer is greater than 0
+			// and we are not at empty char
+            while(j>=0 && s[j] != ' '){
+                counter++;
+				
+				// decrement pointer
+                j--;
+            }
+            
+            return counter;
+        }
+};
+
+int main(){
+
+    Solution solution;
+    string s = "a ";
+
+    int answer = solution.lengthOfLastWord(s);
+
+    cout << "Answer is: " << answer << endl;
+    return 0;
+}
