73c2e2308499ced11370aa89ddfb2a47e7fa1c5b
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-17 12:58:43 +0530

    solved in linear time-correct order

--- Problems/283. Move Zeroes.cpp
+++ Problems/283. Move Zeroes.cpp
@@ -9,36 +9,26 @@ class Solution {
     public:
         void moveZeroes(vector<int>& nums) {
 
+            int i =0;
+            int j =0;
 
-            int i;
-            int j = nums.size()-1;
-
-            for(int k = nums.size()-1;k>=0;k--){
-                if(nums[k] == 0){
-                    i = k;
-                    break;
-                }
-            }
-
-
-            while(i >= 0 && j >= 0){
-
-                if(nums[j] == 0){
-                    j--;
+            while(i<nums.size() && j<nums.size()){
+                
+                if(nums[i] == 0 && nums[j] == 0){
+                    j++;
                     continue;
-                } 
-
-                swap(nums[i],nums[j]);
-
-                if(nums[i] == 0){
-                    j--;
                 }
-    
-                i--;
+
+                if(nums[i] == 0 &&  nums[j] != 0){
+                    swap(nums[i],nums[j]);
+
+                }
+
+                i++;
+                j++;
 
             }
 
-            
             
         }
 };
@@ -56,7 +46,8 @@ int main(){
 
     Solution solution;
 
-    // vector<int> input = {0,1,0,3,12};
+    // vector<int> input = {1};
+    // vector<int> input = {1,2,3,4};
     vector<int> input = {0,2,3,4};
 
 
