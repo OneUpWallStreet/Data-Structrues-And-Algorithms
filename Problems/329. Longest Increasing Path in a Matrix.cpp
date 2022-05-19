34a555d891afd871bbdb420b9c9c456dba959b84
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-19 17:25:30 +0530

    solved using dynamic programming

--- Problems/329. Longest Increasing Path in a Matrix.cpp
+++ Problems/329. Longest Increasing Path in a Matrix.cpp
@@ -1,7 +1,5 @@
 #include<vector>
-#include<unordered_set>
 #include<iostream>
-#include<queue>
 
 using namespace std;
 
@@ -10,52 +8,49 @@ struct Node{
     int y;
 };
 
-struct pair_hash {
-    inline std::size_t operator()(const std::pair<int,int> & v) const {
-        return v.first*31+v.second;
-    }
-};
 
 class Solution {
     public:
-
-        void printVector(vector<int> nums){
-            for(int i=0;i<nums.size();i++){
-                cout << " " << nums[i] << " ";
-            }
-            cout << endl;
-        }
-
         int longestIncreasingPath(vector<vector<int>>& matrix) {
-            
+            int longestPath = 0;
+
             noOfRows = matrix.size();
             noOfColumns = matrix[0].size();
-
 
             initValueMatrix();
 
             for(int i=0;i<matrix.size();i++){
                 for(int j=0;j<matrix[i].size();j++){
-                    if(valueMatrix[i][j] == -1){
-                        depthFirstSearch(matrix,{i,j},{});
-                    }
-                }
-            }
-
-
-            for(int i=0;i<valueMatrix.size();i++){
-                printVector(valueMatrix[i]);   
-                for(int j=0;j<valueMatrix.size();j++){
-                    longestPath = max(longestPath,valueMatrix[i][j]);
+                    longestPath = max(longestPath,depthFirstSearch(matrix,{i,j}));
                 }
             }
 
             return longestPath;
         }
+
     private:
 
+        int depthFirstSearch(vector<vector<int>>& matrix,Node node){
+
+            if(valueMatrix[node.x][node.y] != -1){
+                return valueMatrix[node.x][node.y];
+            }
+
+            int pathValue = 1;
+            
+            vector<Node> validNodes = fetchValidNodes(matrix,node);
+
+            for(int i=0;i<validNodes.size();i++){
+                pathValue = max(pathValue,1 + depthFirstSearch(matrix,validNodes[i]));
+            }
+
+            valueMatrix[node.x][node.y] = pathValue;
+
+            return pathValue;
+
+        }
+
         vector<vector<int>> valueMatrix;
-        int longestPath = 0;
         int noOfRows;
         int noOfColumns;
     
@@ -98,26 +93,6 @@ class Solution {
 
         }
 
-        void depthFirstSearch(vector<vector<int>>& matrix, Node node,vector<int> path){ 
-
-            vector<Node> validNodes = fetchValidNodes(matrix,node);
-            path.push_back(matrix[node.x][node.y]); 
-
-            int currentPathLength = path.size();
-
-            valueMatrix[node.x][node.y] = max(currentPathLength,valueMatrix[node.x][node.y]);
-
-            for(int i=0;i<validNodes.size();i++){
-                if(valueMatrix[validNodes[i].x][validNodes[i].y] == -1){
-                    depthFirstSearch(matrix,validNodes[i],path);
-                    // valueMatrix[node.x][node.y] = max(currentPathLength+valueMatrix[validNodes[i].x][validNodes[i].y],valueMatrix[node.x][node.y]);
-                }
-                else{
-                    valueMatrix[node.x][node.y] = max(currentPathLength+valueMatrix[validNodes[i].x][validNodes[i].y],valueMatrix[node.x][node.y]);
-                }
-            }
-        }
-
 
 };
 
@@ -127,15 +102,10 @@ class Solution {
 int main(){
 
     Solution solution;
-// [[7,6,1,1],[2,7,6,0],[1,3,5,1],[6,6,3,2]]
 
-    vector<vector<int>> input = {{7,6,1,1}, {2,7,6,0}, {1,3,5,1}, {6,6,3,2}};
-    // vector<vector<int>> input = {{9,9,4},{6,6,8},{2,1,1}};
-
+    vector<vector<int>> input = {{9,9,4},{6,6,8},{2,1,1}};
     int answer = solution.longestIncreasingPath(input);
-
     cout << "Answer is: " << answer << endl;
-
 
     return 0;
 }
