084cab65774d73322d976ddd86ea4469e29325a8
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-22 19:04:58 +0530

    solved using slow & fast pointer

--- Problems/141. Linked List Cycle.java
+++ Problems/141. Linked List Cycle.java
@@ -1,39 +1,45 @@
 import java.util.Set;
 import java.util.HashSet;
 
- class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) {
-          val = x;
-          next = null;
-      }
-  }
-
-  class Solution {
-
-      private Set<ListNode> set = new HashSet<>();
-      public boolean hasCycle(ListNode head) {
-
-          if(head == null){
-              return false;
-          }
-
-          ListNode cur = head;
-
-          while(true){
-
-              if(set.contains(cur)){
-                  return true;
-              }
-
-              set.add(cur);
-              if(cur.next == null){
-                  break;
-              }
-              cur = cur.next;
-          }
-
-        return false;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
+}
+
+
+class Solution {
+
+    private Set<ListNode> set = new HashSet<>();
+
+    public boolean hasCycle(ListNode head) {
+
+        if(head==null){
+            return  false;
+        }
+        else if (head.next == null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(true){
+
+            if(slow == fast){
+                return true;
+            }
+            if(fast.next == null || fast.next.next == null){
+                return  false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            
+        }
+  }
 }
