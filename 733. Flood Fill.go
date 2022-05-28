205e12208b62cf58b79f724d48a6a459da9d00a5
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-28 15:10:54 +0530

    still working on it

---
+++ 733. Flood Fill.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	x int
+	y int
+}
+
+var alreadyVisited = map[Node]bool{}
+
+func nextValidNodes(image *[][]int, node Node, oldColor int) []Node {
+
+	var validNodes []Node
+
+	// Go Bottom
+	if node.x+1 < len(*image) && (*image)[node.x+1][node.y] == oldColor && alreadyVisited[Node{node.x + 1, node.y}] == false {
+		validNodes = append(validNodes, Node{node.x + 1, node.y})
+	}
+
+	// Go Top
+	if node.x-1 >= 0 && (*image)[node.x-1][node.y] == oldColor && alreadyVisited[Node{node.x - 1, node.y}] == false {
+		validNodes = append(validNodes, Node{node.x - 1, node.y})
+	}
+
+	// Go Right
+	if node.y+1 < len((*image)[node.x]) && (*image)[node.x][node.y+1] == oldColor && alreadyVisited[Node{node.x, node.y + 1}] == false {
+		validNodes = append(validNodes, Node{node.x, node.y + 1})
+	}
+
+	// Go Left
+	if node.y-1 >= 0 && (*image)[node.x][node.y-1] == oldColor && alreadyVisited[Node{node.x, node.y - 1}] == false {
+		validNodes = append(validNodes, Node{node.x, node.y - 1})
+	}
+
+	return validNodes
+
+}
+
+func depthFirstSearch(image *[][]int, node Node, oldColor int, newColor int) {
+
+	(*image)[node.x][node.y] = newColor
+
+	nextNodes := nextValidNodes(image, node, oldColor)
+	alreadyVisited[node] = true
+
+	if len(nextNodes) == 0 {
+		return
+	}
+
+	for i := 0; i < len(nextNodes); i++ {
+		depthFirstSearch(image, nextNodes[i], oldColor, newColor)
+	}
+
+}
+
+func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+	depthFirstSearch(&image, Node{sr, sc}, image[sr][sc], newColor)
+	return image
+}
+
+func main() {
+	var input = [][]int{{0, 0, 0}, {0, 0, 0}}
+	answer := floodFill(input, 0, 0, 2)
+	fmt.Println("Slice: ", answer)
+}
