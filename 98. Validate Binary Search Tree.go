913e34aafa46f55addee0e9b8de2ede764453a20
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-29 08:04:13 +0530

    solved it

--- 98. Validate Binary Search Tree.go
+++ 98. Validate Binary Search Tree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type TreeNode struct {
@@ -12,36 +13,24 @@ type TreeNode struct {
 
 func depthFirstTraversal(root *TreeNode, min, max int) bool {
 
-	if root == nil || (root.Left == nil && root.Right == nil) {
+	if root == nil {
 		return true
-	} else if root.Left == nil {
-		return (root.Right.Val > root.Val && root.Right.Val > min && root.Right.Val < max)
-	} else if root.Right == nil {
-		return root.Left.Val < root.Val && root.Left.Val < min
-	} else if root.Left.Val >= root.Val || root.Right.Val <= root.Val || root.Left.Val > min || root.Right.Val < max {
-		fmt.Println("Killed here")
-		fmt.Println("Root val: ", root.Val)
-		fmt.Println("Left val: ", root.Left.Val)
-		fmt.Println("Right val: ", root.Right.Val)
-		fmt.Printf("min: %v & max: %v \n", min, max)
+	} else if root.Val <= min || root.Val >= max {
 		return false
+	} else if root.Left == nil && root.Right == nil {
+		return true
 	}
 
-	return depthFirstTraversal(root.Left, root.Left.Val, max) && depthFirstTraversal(root.Right, min, root.Right.Val)
+	return depthFirstTraversal(root.Left, min, root.Val) && depthFirstTraversal(root.Right, root.Val, max)
 
 }
 
 func isValidBST(root *TreeNode) bool {
-	return depthFirstTraversal(root, root.Left.Val, root.Right.Val)
+	return depthFirstTraversal(root, -math.MaxInt64, math.MaxInt64)
 }
 
 func main() {
-
 	node := &TreeNode{2, &TreeNode{1, nil, nil}, &TreeNode{3, nil, nil}}
-
 	ans := isValidBST(node)
-
 	fmt.Println("Answer is: ", ans)
-
-	// fmt.Println("Beast")
 }
