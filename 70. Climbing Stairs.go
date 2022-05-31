8ca8e8c4f847530262e0d52a293bdcc7254bc888
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-05-31 12:15:33 +0530

    solved using cache on my own yay!

--- 70. Climbing Stairs.go
+++ 70. Climbing Stairs.go
@@ -8,21 +8,33 @@ var totalSteps int = 0
 var cache map[int]int
 
 func recursiveSoln(n int) {
-	if n == 0 {
+
+	if _, didFind := cache[n]; didFind {
+		totalSteps += cache[n]
+		return
+	} else if n == 0 {
 		totalSteps++
 		return
 	}
+
 	if n-1 >= 0 {
 		recursiveSoln(n - 1)
 	}
 	if n-2 >= 0 {
 		recursiveSoln(n - 2)
 	}
+
 }
+
 func climbStairs(n int) int {
-	totalSteps = 0
 	cache = map[int]int{}
-	recursiveSoln(n)
+
+	for i := 1; i <= n; i++ {
+		totalSteps = 0
+		recursiveSoln(i)
+		cache[i] = totalSteps
+	}
+
 	return totalSteps
 }
 
