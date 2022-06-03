0568399faa3ca067a2ca48bfc90422e8d4740b4d
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-03 19:23:16 +0530

    brute force soln

---
+++ 304. Range Sum Query 2D - Immutable.java
@@ -0,0 +1,19 @@
+class NumMatrix {
+
+    int[][] matrix = new int[][]{};
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        int sum = 0;
+
+        for (int i = row1;i<=row2;i++){
+            for(int j=col1;j<=col2;j++){
+                sum += matrix[i][j];
+            }
+        }
+        return  sum;
+    }
+}
