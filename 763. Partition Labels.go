8a4a9eefc33fde61bc22c6520e399f734a5e3ab0
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-04 20:06:28 +0530

    solved but look agn

--- 763. Partition Labels.go
+++ 763. Partition Labels.go
@@ -4,27 +4,44 @@ import (
 	"fmt"
 )
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func partitionLabels(s string) []int {
 
-	freqMap := map[byte]bool{}
-
+	freqMap := map[byte]int{}
 	result := []int{}
-	counter := 0
+
+	// Init map to keep track of last element
+	for i := 0; i < len(s); i++ {
+		freqMap[s[i]] = i
+	}
+
+	size := 0
+	end := freqMap[s[0]]
 
 	for i := 0; i < len(s); i++ {
-		if freqMap[s[i]] == false {
-			freqMap[s[i]] = true
-			counter++
-		} else {
-			result = append(result, counter)
-			counter = 0
-			freqMap = map[byte]bool{}
+		size++
+		end = max(end, freqMap[s[i]])
+
+		if i == end {
+			result = append(result, size)
+			size = 0
 		}
+
 	}
 
 	return result
 }
 
 func main() {
+
+	answer := partitionLabels("ababcbacadefegdehijhklij")
+	fmt.Println("Answer : ", answer)
+
 	fmt.Println("Warren Buffet")
 }
