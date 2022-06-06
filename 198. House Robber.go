eea0be40be9af3662e67d3514989bc3460a1ccd2
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-06 18:51:31 +0530

    still wrong

--- 198. House Robber.go
+++ 198. House Robber.go
@@ -15,17 +15,13 @@ func dynamicProgrammingSolution(index, loot int, nums []int, cache map[int]int) 
 
 	loot += nums[index]
 
-	// fmt.Println(loot)
-
 	if _, didFind := cache[index]; didFind {
-		// fmt.Printf("cache Val: %v and loot val: %v  and index: %v \n", cache[index], loot, index)
 		return cache[index]
 	}
 
-	// if index == len(nums)-1 {
-	// 	cache[index] = loot
-	// 	return loot
-	// }
+	if index >= len(nums) {
+
+	}
 
 	maxVal := loot
 	for i := index + 2; i < len(nums); i++ {
@@ -33,7 +29,20 @@ func dynamicProgrammingSolution(index, loot int, nums []int, cache map[int]int) 
 	}
 
 	cache[index] = maxVal
+
 	return maxVal
+}
+
+func rob(nums []int) int {
+	var loot int
+	cache := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		loot = max(loot, dynamicProgrammingSolution(i, 0, nums, cache))
+	}
+
+	printCache(cache)
+
+	return loot
 }
 
 func printCache(cache map[int]int) {
@@ -46,19 +55,7 @@ func printCache(cache map[int]int) {
 
 }
 
-func rob(nums []int) int {
-	var loot int
-	cache := map[int]int{}
-	for i := len(nums) - 1; i >= 0; i-- {
-		// fmt.Println("\n root index: ", i)
-		loot = max(loot, dynamicProgrammingSolution(i, 0, nums, cache))
-		printCache(cache)
-	}
-
-	return loot
-}
-
 func main() {
-	ans := rob([]int{1, 2, 3, 1})
+	ans := rob([]int{1, 2, 1, 1})
 	fmt.Println("answer is: ", ans)
 }
