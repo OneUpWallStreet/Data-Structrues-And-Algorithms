071d615cb7ad8efe118a1c51efdb5d6b3cc6eb16
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-09 17:43:44 +0530

    linear time constant space

--- 41. First Missing Positive.go
+++ 41. First Missing Positive.go
@@ -1,36 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
+func abs(x int) int {
+	return int(math.Abs(float64(x)))
 }
 
 func firstMissingPositive(nums []int) int {
 
-	freqMap := map[int]bool{}
-
-	var maxNum int
-
+	// Replace all the negative numbers with 0
 	for i := 0; i < len(nums); i++ {
-		maxNum = max(maxNum, nums[i])
-		freqMap[nums[i]] = true
+		if nums[i] < 0 {
+			nums[i] = 0
+		}
 	}
 
-	for i := 1; i <= maxNum+1; i++ {
-		if _, didFind := freqMap[nums[i]]; !didFind {
+	// Traverse array again and mark elements
+	for i := 0; i < len(nums); i++ {
+
+		// Check for out of bounds
+		if abs(nums[i])-1 < 0 || abs(nums[i])-1 >= len(nums) {
+			continue
+		}
+
+		// We have alraedy marked the element
+		if nums[abs(nums[i])-1] < 0 {
+			continue
+		}
+
+		if nums[abs(nums[i])-1] == 0 {
+			nums[abs(nums[i])-1] = -(len(nums) + 1)
+		} else {
+			nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
+		}
+
+	}
+
+	for i := 1; i <= len(nums); i++ {
+		if nums[i-1] >= 0 {
 			return i
 		}
 	}
 
-	return -1
+	return len(nums) + 1
 
 }
 
 func main() {
-	ans := firstMissingPositive([]int{3, 4, -1, 1})
+	ans := firstMissingPositive([]int{0})
 	fmt.Println(ans)
 }
