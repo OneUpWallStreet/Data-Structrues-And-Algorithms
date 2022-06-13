f8736aeab814c3bbfa2e919d3605f0869f3ab65d
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-13 20:36:31 +0530

    solved but got TLE

--- 120. Triangle.go
+++ 120. Triangle.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func min(x, y int) int {
 	if x < y {
@@ -9,30 +12,43 @@ func min(x, y int) int {
 	return y
 }
 
-func selectMinFromRow(row []int, index int) (int, int) {
+func fetchNextCells(row []int, index int) []int {
+
+	nextCells := []int{}
+
 	if index+1 >= len(row) {
-		return row[index], index
-	} else if row[index] < row[index+1] {
-		return row[index], index
+		nextCells = append(nextCells, index)
 	} else {
-		return row[index+1], index + 1
+		nextCells = append(nextCells, index)
+		nextCells = append(nextCells, index+1)
 	}
+
+	return nextCells
+
+}
+
+func recursiveTraversal(traingle [][]int, rowIndex, colIndex, curSum int) int {
+	curSum += traingle[rowIndex][colIndex]
+	rowIndex++
+	if rowIndex >= len(traingle) {
+		fmt.Println(curSum)
+		return curSum
+	}
+	nextCells := fetchNextCells(traingle[rowIndex], colIndex)
+	var minRes int = math.MaxInt64
+	for i := 0; i < len(nextCells); i++ {
+		minRes = min(recursiveTraversal(traingle, rowIndex, nextCells[i], curSum), minRes)
+	}
+	return minRes
 }
 
 func minimumTotal(triangle [][]int) int {
-
-	result := triangle[0][0]
-	currentIndex := 0
-	var minElement int
-
-	for i := 1; i < len(triangle); i++ {
-		row := triangle[i]
-		minElement, currentIndex = selectMinFromRow(row, currentIndex)
-		result += minElement
-	}
-
-	return result
+	return recursiveTraversal(triangle, 0, 0, 0)
 }
+
+//      -1
+//    2   3
+//  1   -1   3
 
 func main() {
 
