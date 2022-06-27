37a81fb856ca6bc6e35c5944d8203b9eaabbe016
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-27 12:29:41 +0530

    solved again

--- 98. Validate Binary Search Tree.swift
+++ 98. Validate Binary Search Tree.swift
@@ -1,39 +1,55 @@
 import Foundation
 
 //  Definition for a binary tree node.
-  public class TreeNode {
-      public var val: Int
-      public var left: TreeNode?
-      public var right: TreeNode?
-      public init() { self.val = 0; self.left = nil; self.right = nil; }
-      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-          self.val = val
-          self.left = left
-          self.right = right
-      }
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+      self.val = val
+      self.left = left
+      self.right = right
   }
+}
 
-
+ 
 class Solution {
     
-    func depthFirstTraversal(node: TreeNode?, leftBoundry: Int, rightBoundry: Int) -> Bool{
-        
-        if(node == nil){
+    private func recursiveValidation(node: TreeNode?,max: Int, min: Int) -> Bool {
+
+        // If node is nil, than of-course it's valid
+        guard let node = node else {
             return true
         }
-        
-        
-        if(!(node!.val > leftBoundry && node!.val < rightBoundry)){
+       
+       // Check if current node is valid
+        if node.val >= max || node.val <= min {
             return false
         }
         
+        // Both children are nil
+        if node.left == nil && node.right == nil {
+            return true
+        }
         
-        return (depthFirstTraversal(node: node?.left, leftBoundry: leftBoundry, rightBoundry: node!.val) && depthFirstTraversal(node: node?.right, leftBoundry: node!.val, rightBoundry: rightBoundry))
+        // Only left subtree is exists
+        else if node.left != nil && node.right == nil{
+            return recursiveValidation(node: node.left, max: node.val, min: min)
+        }
+        
+        // Only Right subtree is exists
+        else if node.right != nil && node.left == nil {
+            return recursiveValidation(node: node.right, max: max, min: node.val)
+        }
+
+        // Both children exist! Yay
+        return recursiveValidation(node: node.left, max: node.val, min: min) && recursiveValidation(node: node.right, max: max, min: node.val)
         
     }
     
-    func isValidBST(_ root: TreeNode?) -> Bool {
-        return depthFirstTraversal(node: root, leftBoundry: Int.min, rightBoundry: Int.max)
-    }
+   func isValidBST(_ root: TreeNode?) -> Bool {
+       return recursiveValidation(node: root, max: Int.max, min: Int.min)
+   }
 }
