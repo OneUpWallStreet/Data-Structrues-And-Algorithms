53a4e627a16b309ad94d6024fb3d8ce6a7ecfbb8
OneUpWallStreet <oneupwallstreet@gmail.com>
2022-06-29 16:50:47 +0530

    solved using dfs

--- 797. All Paths From Source to Target.py
+++ 797. All Paths From Source to Target.py
@@ -1,19 +1,14 @@
-import copy
 from typing import List
-import collections
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        rows = len(graph)
-        cols = len(graph[0])
         result = []
-        q = collections.deque()
-        q.append((0,[]))
-        while q:
-            cur,path = q.popleft()
-            path.append(cur)
-            if cur == len(graph)-1:
+        def dfs(node: int,path: List[int]):
+            if node == len(graph)-1:
                 result.append(path[:])
-            for node in graph[cur]:
-                q.append((node,copy.deepcopy(path)))
+            for nextNode in graph[node]:
+                path.append(nextNode)
+                dfs(nextNode,path)
+                path.pop()
+        dfs(0,[0])
         return result
