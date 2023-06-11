33f90a289302237d29519affb0b95933ddd1d9df
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-06-11 16:38:01 +0530

    better soln

--- 733. Flood Fill.py
+++ 733. Flood Fill.py
@@ -1,33 +1,15 @@
-from typing import List
-
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        rows = len(image) - 1
-        cols = len(image[0]) - 1
-        startingColor = image[sr][sc]
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        rows, cols = len(image), len(image[0])
+        startColor = image[sr][sc]
+        if image[sr][sc] == color: return image
 
-        if image[sr][sc] == color:
-            return image
-
-        def dfs(r, c):
-
-            if r < 0 or c < 0 or r > rows or c > cols or image[r][c] != startingColor:
-                return
-
+        def dfs(r,c):
             image[r][c] = color
-
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-
-        dfs(sr, sc)
+            for dr,dc in directions:
+                if r+dr in range(rows) and c+dc in range(cols) and image[r+dr][c+dc] == startColor:
+                    dfs(r+dr,c+dc)
+        dfs(sr,sc)
         return image
-
-
-if __name__ == '__main__':
-    solution = Solution()
-    answer = solution.floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2)
-    print("Answer: ", answer)
