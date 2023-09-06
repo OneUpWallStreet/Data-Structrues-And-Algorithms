e1044739432cd936f9d33fad15a0e14a72aed858
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-09-06 14:05:26 -0700

    solved in 2023

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -11,3 +11,16 @@ class Solution:
             cache[n] = dfs(n-1) + dfs(n-2)
             return cache[n]
         return dfs(n)
+    
+    def climbStairsSemp2023(self, n: int) -> int:
+
+        result, cache = 0, dict() 
+
+        def dfs(step) -> int:
+            if step == n: return 1
+            elif step > n: return 0 
+            elif step in cache:  return cache[step]
+            cache[step] = dfs(step+1) + dfs(step+2)
+            return cache[step]            
+
+        return dfs(0)
