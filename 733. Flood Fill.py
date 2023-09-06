7134ce4a15a1e0e529f2b3afa417c273b7b7e417
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-09-05 17:13:27 -0700

    solved using BFS

--- 733. Flood Fill.py
+++ 733. Flood Fill.py
@@ -13,3 +13,26 @@ class Solution:
                     dfs(r+dr,c+dc)
         dfs(sr,sc)
         return image
+
+
+# BFS 
+    def BFSfloodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+
+        rows, cols, q, ogColor, visited = len(image), len(image[0]), collections.deque(), image[sr][sc], set()
+
+        q.append((sr,sc))
+
+        while q:
+            
+            r,c = q.popleft()
+            visited.add((r,c))
+            image[r][c] = color
+
+            for dr,dc in directions:
+                if r+dr in range(rows) and c+dc in range(cols) and image[r+dr][c+dc] == ogColor and (r+dr,c+dc) not in visited:
+                    q.append((r+dr,c+dc))
+
+        return image
+        
