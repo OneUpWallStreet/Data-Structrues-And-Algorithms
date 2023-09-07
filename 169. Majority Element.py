9d039f698a58242501cd77de18ebeb301d621946
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-09-07 15:52:03 -0700

    needed neetcode for O(1) soln

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,4 +1,6 @@
 from typing import List
+import heapq
+import collections
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
@@ -15,4 +17,21 @@ class Solution:
             if value > len(nums)/2:
                 return key
             
+    def heapSolutionmajorityElement(self, nums: List[int]) -> int:        
+        heap, freqMap = [], collections.defaultdict(int)
+        for num in nums: freqMap[num] += 1
+        for k,v in freqMap.items(): heapq.heappush(heap,(-1*v,k))
+        return heapq.heappop(heap)[1]
         
+    def BoyerMooreAlgorithmMajorityElement(self, nums: List[int]) -> int:
+        
+        result = counter = 0 
+
+        for num in nums:
+            if num == result: counter += 1
+            elif num != result and counter > 0: counter -= 1
+            else: 
+                result = num
+                counter += 1
+
+        return result
