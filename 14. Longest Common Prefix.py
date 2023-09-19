c365f9dbc5bd679fa1ba384cd288e02c57cf83a8
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-09-19 15:24:13 -0700

    solved problem using Trie

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,3 +1,44 @@
+from typing import List
+
+class Node:
+    
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class TrieSolution:
+
+    def __init__(self):
+        self.result = ""
+        self.root = Node()
+
+    def insertIntoTrie(self,word: str):
+        cur = self.root
+        for ch in word:
+            if ch in cur.children: 
+                cur = cur.children[ch]
+                continue
+            else: cur.children[ch] = Node()
+            cur = cur.children[ch]
+        cur.isEnd = True
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        for word in strs:
+            if word == "": return ""
+            self.insertIntoTrie(word)
+        
+        # Traverse trie root and find answer
+        cur = self.root
+        while len(cur.children) == 1:
+            val = list(cur.children)[0]
+            if cur.isEnd: break
+            self.result += val
+            cur = cur.children[val]
+        
+        return self.result
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
