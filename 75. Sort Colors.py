1e70c7c59ddad2da2cba4e293a4d2a45c76c57a0
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-04 19:56:21 -0700

    Solved using bucket

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -1,6 +1,32 @@
 import collections
+from typing import List
 
 class Solution:
+
+    def sortColorsBucket(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        bucket = [0,0,0]
+
+        for num in nums: bucket[num] += 1
+
+        print(bucket)
+
+        bi = 0
+        index = 0
+        
+        while index < len(nums):
+            if bucket[bi] > 0:
+                bucket[bi] -= 1
+                nums[index] = bi
+            else:
+                bi += 1
+                continue 
+    
+            index += 1
+
     def sortColors(self, nums: List[int]) -> None:
         
         hashmap = collections.defaultdict(int)
