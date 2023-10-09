914eb55c0d883781d135efcb28ac67137c43d2b2
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-08 18:07:47 -0700

    better code

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,17 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-
-        stack = collections.deque()
-        hashset = ("{", "[","(")
-
-        def match(p1,p2):
-            if p1 == "{"  and p2 == "}": return True
-            elif p1 == "("  and p2 == ")": return True
-            elif p1 == "["  and p2 == "]": return True
-            else: return False
         
-        for ch in s:
-            if ch in hashset: stack.append(ch) 
-            elif len(stack) == 0 or match(stack.pop(),ch) == False: return False
+        stack = collections.deque()
 
-        return len(stack) == 0
+        for ch in s:
+            if ch == "(": stack.append(")")
+            elif ch == "[": stack.append("]")
+            elif ch == "{": stack.append("}")
+            else: 
+                if not stack or stack.pop() != ch: return False 
+    
+        return not stack
