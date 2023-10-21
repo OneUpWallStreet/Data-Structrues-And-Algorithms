228e4fa34db6d4b6ac063dae5c3fe0b8c0043b39
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-20 21:55:28 -0700

    better soln

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -17,3 +17,12 @@ class Solution:
                 last -= 1
         
         return result
+
+    # Simpler Code 
+    def maxArea2023(self, height: List[int]) -> int:
+        l, r, area = 0, len(height) - 1, float('-inf')
+        while l < r:
+            area = max(area, min(height[l],height[r])*(r-l))
+            if height[l] < height[r]: l += 1
+            else: r -= 1
+        return area
