c9c5542ab1216a048694070abf5ceac9659ffcad
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-24 12:01:05 -0700

    dummy node trick

---
+++ 19. Remove Nth Node From End of List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        dummy = ListNode()
+        dummy.next = head
+        left,right = dummy, head
+        
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right != None: 
+            right = right.next
+            left = left.next
+        
+        left.next = left.next.next
+        return dummy.next
