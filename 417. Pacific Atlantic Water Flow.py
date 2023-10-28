de82fec77d5aad0e0b240109f6ea5e386c300874
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-28 15:48:43 -0700

    qsn made me switch to py

---
+++ 417. Pacific Atlantic Water Flow.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        directions = [[1,0],[0,1],[0,-1],[-1,0]]
+
+        rows, cols = len(heights), len(heights[0])
+        used = set()
+        result = []
+
+        def dfs(r,c,isForPacific):
+            used.add((r,c))
+            for dr,dc in directions:
+                if isForPacific == True and (r+dr < 0 or c + dc < 0):  return True
+                elif isForPacific == False and (r+dr >= rows or c + dc >= cols):  return True
+                elif r+dr in range(rows) and c + dc in range(cols) and heights[r+dr][c+dc] <= heights[r][c] and (r+dr,c+dc) not in used:
+                    if dfs(r+dr,c+dc,isForPacific): return True
+            return False
+
+        for r in range(rows):
+            for c in range(cols):
+                used = set()
+                pac = dfs(r,c,True)
+                used = set()
+                atl = dfs(r,c,False)
+                if pac and atl: result.append([r,c])        
+        return result
