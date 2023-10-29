222e6d5262eb6eb0c0d82057f988540185600759
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-10-28 17:08:34 -0700

    smarter soln

--- 417. Pacific Atlantic Water Flow.py
+++ 417. Pacific Atlantic Water Flow.py
@@ -24,3 +24,34 @@ class Solution:
                 atl = dfs(r,c,False)
                 if pac and atl: result.append([r,c])        
         return result
+
+    def pacificAtlanticBetter(self, heights: List[List[int]]) -> List[List[int]]:
+        
+
+        directions = [[1,0],[0,1],[0,-1],[-1,0]]
+
+        rows, cols = len(heights), len(heights[0])
+        pac, atl = set(), set()
+
+        def dfs(r,c,hs):
+
+            hs.add((r,c))
+
+            for dr, dc in directions:
+                if r+dr in range(rows) and c+dc in range(cols) and heights[r+dr][c+dc] >= heights[r][c] and (r+dr,c+dc) not in hs:
+                    dfs(r+dr,c+dc,hs)
+
+        for c in range(cols):
+            dfs(0,c,pac)
+            dfs(rows-1,c,atl)
+
+        for r in range(rows):
+            dfs(r,0,pac)
+            dfs(r,cols-1,atl)
+
+        result = []
+
+        for val in pac:
+            if val in atl: result.append(val)
+
+        return result
