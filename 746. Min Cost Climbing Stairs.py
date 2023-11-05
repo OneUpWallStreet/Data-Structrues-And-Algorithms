3e3035cae102c26e705c8389486daaf01f5cb039
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-11-05 15:26:49 -0800

    another soln

--- 746. Min Cost Climbing Stairs.py
+++ 746. Min Cost Climbing Stairs.py
@@ -14,3 +14,16 @@ class Solution:
             cache[index] = cost[index] + min(dfs(index-1),dfs(index-2))
             return cache[index]
         return min(dfs(len(cost)-1),dfs(len(cost)-2))
+
+    def forwardMinCostClimbingStairs(self, cost: List[int]) -> int:
+
+        cache = dict()
+
+        def rcr(index):
+            if index in cache: return cache[index]
+            if index >= len(cost): return 0
+            elif index == len(cost)-1 or index == len(cost)-2: return cost[index]
+            cache[index] = min(cost[index] + rcr(index+1), cost[index] + rcr(index+2))
+            return cache[index]
+        
+        return min(rcr(0),rcr(1))
