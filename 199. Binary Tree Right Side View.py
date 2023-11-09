e32fb85ed60ae3156210c7930fcb72a7988f9831
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-11-09 12:56:38 -0800

    added bfs & dfs

--- 199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View.py
@@ -1,6 +1,52 @@
 import collections
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
+
+    def BFSLevelOrderRightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        result = []
+        q = collections.deque()
+        tree = []
+        q.append(root)
+        while q:
+            level = []
+            for i in range(len(q)):
+                cur = q.popleft()
+                level.append(cur.val)
+                if cur.left: q.append(cur.left)
+                if cur.right: q.append(cur.right)
+            tree.append(level)
+        for level in tree: result.append(level[-1])
+        return result
+
+
+    def BetterDFSRightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        depth = 0
+        levels = collections.defaultdict(list)
+        res = []
+
+        def dfs(node,curLevel):
+            nonlocal depth
+            if node == None: return
+            levels[curLevel].append(node.val)
+            curLevel += 1
+            depth = max(depth,curLevel)
+            dfs(node.left,curLevel)
+            dfs(node.right,curLevel)
+
+        dfs(root,0)
+
+        for i in range(depth): res.append(levels[i][-1])
+        return res
+
+#   This is from July 2022
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
         levels = collections.defaultdict(list)
