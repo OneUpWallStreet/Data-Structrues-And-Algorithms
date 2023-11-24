0a64e7e6b912dbfcb6d91f771a7391b5b597cd99
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-11-23 19:15:41 -0800

    better soln

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -1,21 +1,21 @@
+from typing import Optional
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+# Better Solution
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if root == None: return False
-        def dfs(node,curSum):
-            if node == None: return False
+        targetSum -= root.val
+        if root.left == None and root.right == None and targetSum == 0: return True 
+        if root.left != None:
+            if self.hasPathSum(root.left,targetSum): return True
+        if root.right != None:
+            if self.hasPathSum(root.right,targetSum): return True
+        return False
 
-            curSum += node.val
-
-            if not node.left and not node.right and curSum == targetSum: return True
-            elif node.left and node.right: return dfs(node.left,curSum) or dfs(node.right,curSum)
-            elif node.left: return dfs(node.left,curSum)
-            elif node.right: return dfs(node.right,curSum)
-
-        return dfs(root,0)
