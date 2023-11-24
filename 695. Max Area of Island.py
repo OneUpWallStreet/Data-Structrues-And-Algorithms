0d0fe7e52d5d0912708f6db1bddd0c2ce03c8df9
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-11-24 06:29:07 -0800

    better soln

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -1,34 +1,26 @@
 from typing import List
 
 class Solution:
-    maxArea = 0
-    currentIslandArea = 0
-
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
 
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        alreadyVisited = set()
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        result = 0
 
-        def dfs(r, c):
+        def dfs(r,c):
+            visited.add((r,c))
+            for dr,dc in directions:
+                if r + dr in range(rows) and c+dc in range(cols) and grid[r+dr][c+dc] == 1 and (r+dr,c+dc) not in visited:
+                    dfs(r+dr,c+dc)
 
-            self.currentIslandArea += 1
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in visited and grid[r][c] == 1: 
+                    s1 = len(visited)
+                    dfs(r,c)
+                    s2 = len(visited)
+                    result = max(result,s2-s1)
 
-            self.maxArea = max(self.maxArea, self.currentIslandArea)
-            alreadyVisited.add((r, c))
-            for dr, dc in directions:
-                if (r + dr) in range(rows) and \
-                        (c + dc) in range(cols) and \
-                        (r + dr, c + dc) not in alreadyVisited and \
-                        grid[r + dr][c + dc] == 1:
-                    dfs(r + dr, c + dc)
-
-        for i in range(0, rows):
-            for j in range(0, cols):
-                if grid[i][j] == 1 and (i, j) not in alreadyVisited:
-                    self.currentIslandArea = 0
-                    dfs(i, j)
-
-        return self.maxArea
-
+        return result
