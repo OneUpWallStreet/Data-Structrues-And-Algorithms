14dc58106be3f2e578ab98ac34ff42773fe0f206
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-12-16 13:39:36 -0800

    annoying

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -1,4 +1,23 @@
 class Solution:
+
+    # From NeetCode
+    def Dev2023gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        len1, len2 = len(str1), len(str2)
+
+        def isDivisor(i):
+            if len1 % len(str1[:i+1]) != 0 or len2 % len(str2[:i+1]) != 0: return None
+            
+            f1, f2 = len1 // len(str1[:i+1]), len2 // len(str2[:i+1])
+
+            if (str1[:i+1] == str2[:i+1]) and (f1 * str1[:i+1] == str1 and f2 * str2[:i+1] == str2): return str1[:i+1]
+
+
+        for i in range(min(len1,len2),-1,-1):
+            cur = isDivisor(i)
+            if cur != None: return cur
+        return ""
+
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
         result = ""
