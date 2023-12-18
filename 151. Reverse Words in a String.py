be8d52f712519ff79a3f92e39d5c85d656f248a0
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-12-17 20:21:02 -0800

    much better soln

--- 151. Reverse Words in a String.py
+++ 151. Reverse Words in a String.py
@@ -1,27 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-
-        words = []
-        cur = ""
-        for i in s:
-            if i == " ":
-                if len(cur) == 0:
-                    continue
-                else:
-                    words.append(cur)
-                    cur = ""
-            else:
-                cur = cur + i
-
-        if len(cur) > 0:
-            words.append(cur)
-
-        output = ""
-
-        for index in range(len(words)-1,-1,-1):
-            if index != 0:
-                output = output + words[index] + " "
-            else:
-                output = output + words[index]
-
-        return output
+        words = s.split(" ")
+        result = []
+        for i in range(len(words)-1,-1,-1):
+            if len(words[i]) == 0: continue
+            result.append(words[i])
+        return " ".join(result)
