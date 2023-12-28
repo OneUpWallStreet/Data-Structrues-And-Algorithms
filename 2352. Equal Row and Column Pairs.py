e17153b3746c4ab9643273b411344509324b31a7
OneUpWallStreet <oneupwallstreet@gmail.com>
2023-12-27 21:17:14 -0800

    my hashing soln

--- 2352. Equal Row and Column Pairs.py
+++ 2352. Equal Row and Column Pairs.py
@@ -18,6 +18,32 @@ class Solution:
                 result += rowHashMap[colStr]
         
         return result
+    
+    # My own hashing solution, this just means that I did not look at other solns lol
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        
+        result = 0
+        rows, cols = len(grid), len(grid[0])
+        rHM, cHM = dict(), dict()
+
+        def insertRow(r):
+            s = ""
+            for i in range(len(grid[r])): s += str(grid[r][i]) + "#"
+            rHM[r] = s
+        
+        def insertCol(c):
+            s = ""
+            for i in range(rows): s += str(grid[i][c]) + "#"
+            cHM[c] = s
+
+        for r in range(rows):
+            for c in range(cols):
+                if r not in rHM: insertRow(r)
+                if c not in cHM: insertCol(c)
+                if rHM[r] == cHM[c]: result += 1
+
+
+        return result
 
     # Came up with it but no hashing 
     def bruteForceSolnequalPairs(self, grid: List[List[int]]) -> int:
