3da1db304c520a507b223f319eac81d1de51b7b4
OneUpWallStreet <oneupwallstreet@gmail.com>
2024-01-09 14:33:16 -0800

    came up with it on my own

--- 2130. Maximum Twin Sum of a Linked List.py
+++ 2130. Maximum Twin Sum of a Linked List.py
@@ -23,4 +23,43 @@ class Solution:
         return result
 
 
+    # Code is not the best, but it works. Its Linear Time & Constant Space
+    # Came up with this solution on my own
+    def LinearTimeConstantSpacepairSum(self, head: Optional[ListNode]) -> int:
+        
+        listSize = 0 
+        cur = head
 
+        while cur != None:
+            cur = cur.next
+            listSize += 1
+        
+        mid = int(listSize/2)
+        cur = head
+        n = 0
+
+        while n != mid:
+            cur = cur.next
+            n += 1
+
+        # Reverse List
+        prev = None
+        
+        while cur != None:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        
+        result = float('-inf')
+        cur = head
+        counter = 0
+
+        while counter != mid:
+            result = max(result,prev.val + cur.val)
+            prev = prev.next
+            cur = cur.next
+            counter += 1
+
+
+        return result
