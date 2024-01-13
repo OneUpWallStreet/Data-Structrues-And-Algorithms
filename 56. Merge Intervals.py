285e5ebf17dd4edee3f7ee6b7b80595fb96e6221
OneUpWallStreet <oneupwallstreet@gmail.com>
2024-01-13 12:41:32 -0800

    much better soln in 5 mins

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,17 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
+
         intervals.sort()
         result = [intervals[0]]
-
-        for cur in intervals:
-            top = result[-1]
-
-            if (cur[0] <= top[1] and cur[1] >= top[1]) or (cur[1] < top[1]):
+        for i in range(1,len(intervals)):
+            cur, top = intervals[i], result[-1]
+            if top[1] >= cur[0]:
                 result.pop()
-                result.append([min(top[0],cur[0]),max(cur[1],top[1])])
+                result.append([top[0],max(cur[1],top[1])])
             else: result.append(cur)
-        
         return result
 
+
+        
+        pass
         
