3739797cceead7fe341139932946537df578d527
OneUpWallStreet <oneupwallstreet@gmail.com>
2024-01-29 11:20:11 -0800

    better soln

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -1,35 +1,36 @@
+import collections
+
 class MyQueue:
 
     def __init__(self):
         self.s1, self.s2 = collections.deque(), collections.deque()
+        
 
-    def push(self, x: int) -> None:
-        self.s1.append(x)
+    def push(self, x: int) -> None: self.s1.append(x)
+        
 
     def pop(self) -> int:
-
-        # Fill S2 with S1 expect for last element
-        # Shift S2 back into S1 
         self.s2.clear()
-        while len(self.s1) > 1:
-            self.s2.append(self.s1.pop()) 
-        ele = self.s1.pop()
-        
+        while len(self.s1) > 1: self.s2.append(self.s1.pop())
+        result = self.s1.pop()
         while self.s2: self.s1.append(self.s2.pop())
-        return ele
+        return result
 
     def peek(self) -> int:
-        
         self.s2.clear()
-        print(self.s1)
-        while len(self.s1) > 1:
-            self.s2.append(self.s1.pop()) 
-        print(self.s1)
-        ele = self.s1.pop()
-        self.s2.append(ele)
+        while len(self.s1) > 1: self.s2.append(self.s1.pop())
+        result = self.s1.pop()
+        self.s2.append(result)
         while self.s2: self.s1.append(self.s2.pop())
-        return ele
+        return result
+
+    def empty(self) -> bool: return not self.s1
         
 
-    def empty(self) -> bool:
-        return len(self.s1) == 0
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
