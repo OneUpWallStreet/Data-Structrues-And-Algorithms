5149410b0c619919bb4be0ff8e2e4352c588f24d
OneUpWallStreet <oneupwallstreet@gmail.com>
2024-03-03 21:32:39 -0800

    easier to understand

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,17 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
+        
         intervals.sort()
         result = [intervals[0]]
+
+        # These functions are so good. Make this problem 10 times easier 
+        def isOverlap(a,b): 
+            return a[0] <= b[1] and a[1] >= b[0]
+        
+        def merge(a,b): 
+            return [min(a[0],b[0]),max(a[1],b[1])]
+
         for i in range(1,len(intervals)):
-            cur, top = intervals[i], result[-1]
-            if top[1] >= cur[0]:
+
+            top = result[-1]
+            cur = intervals[i]
+
+            if isOverlap(top,cur): 
                 result.pop()
-                result.append([top[0],max(cur[1],top[1])])
-            else: result.append(cur)
+                result.append(merge(top,cur))
+                
+            else: result.append(intervals[i])
+        
         return result
-
-
-        
-        pass
-        
