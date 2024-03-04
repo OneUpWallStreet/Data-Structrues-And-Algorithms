7c30ef2f9ee6d9cc7f208d1ea4fa169b997dcae1
OneUpWallStreet <oneupwallstreet@gmail.com>
2024-03-04 11:22:29 -0800

    could solve hard on my own

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -1,35 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(s) < len(t): return ""
         
-        tMap = collections.defaultdict(int)
-        sMap = collections.defaultdict(int)
+        result = None
 
-        for ch in t: tMap[ch] += 1
+        tMap = Counter(t)
 
-        same = 0
-        result = [float('inf'),(0,0)]
+        hm = collections.defaultdict(int)  
 
-        l = r = 0
+        same = l = r = 0
 
         while r < len(s):
-
-            sMap[s[r]] += 1
-
-            if s[r] in tMap and sMap[s[r]] == tMap[s[r]]: same += 1
-            while same == len(tMap):
-                
-                if result[0] > (r-l) + 1: 
-                    result = [(r-l)+1,(l,r)]
-                sMap[s[l]] -= 1
-
-                if s[l] in tMap and sMap[s[l]] < tMap[s[l]]:  same -= 1
-                
-                l += 1
             
+            ch = s[r]
+            hm[ch] += 1
+
+            if hm[ch] == tMap[ch]: same += 1
+
+            while same == len(tMap):
+
+                if not result or (r-l) + 1 < len(result):
+                    result = s[l:r+1]
+
+                hm[s[l]] -= 1
+
+                if hm[s[l]] < tMap[s[l]]: same -= 1
+                l += 1
+
             r += 1
-
-        if result[0] != float('inf'): return s[result[1][0]:result[1][1]+1]
-        else: return ""
-
+        return result if result else ""
