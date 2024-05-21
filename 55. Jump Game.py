82e12050afbb934ac51bf2b553a55aac89fc2ca1
Swarom Muley <oneupwallstreet@gmail.com>
2024-05-21 12:37:54 -0700

    added bottom up dp for DAA

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -4,3 +4,15 @@ class Solution:
         for i in range(len(nums)-1,-1,-1):
             if nums[i] + i >= goal: goal = i
         return True if goal == 0 else False
+    
+    # Bottom-up DP
+    def canJumpDP(PDself, nums: List[int]) -> bool:
+        dp = [False] * len(nums)
+        dp[len(nums)-1] = True
+        for i in range(len(nums)-2,-1,-1):
+            maxJump = min(i+nums[i],len(nums)-1) 
+            for j in range(i+1,maxJump+1):
+                if dp[j]: 
+                    dp[i] = True
+                    break
+        return dp[0]
