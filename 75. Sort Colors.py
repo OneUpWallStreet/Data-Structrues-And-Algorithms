dbd0a2b457cfc8e1ff5db2735cbb5510aca0fe60
Swarom Muley <oneupwallstreet@gmail.com>
2025-02-06 13:28:36 -0800

    cleaner soln

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -1,37 +1,12 @@
-import collections
-from typing import List
-
 class Solution:
-
-    def sortColorsBucket(self, nums: List[int]) -> None:
+    def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-    
-        bucket = [0,0,0]
-
-        for num in nums: bucket[num] += 1
-        index = 0
-        for i in range(3):
-            for j in range(bucket[i]):
-                nums[index] = i
+        freqMap, index = Counter(nums), 0
+         
+        for color in range(3):
+            for _ in range(freqMap[color]):
+                nums[index] = color
                 index += 1
-            
         
-
-    def sortColors(self, nums: List[int]) -> None:
-        
-        hashmap = collections.defaultdict(int)
-        
-        for num in nums:
-            hashmap[num] += 1
-            
-        counter = 0
-        
-        for i in range(3):
-            while hashmap[i] != 0:
-                nums[counter] = i
-                hashmap[i] -= 1
-                counter += 1    
-            
-            
